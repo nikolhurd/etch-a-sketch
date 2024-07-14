9d200cd8a9809a27a3947cfd27f691773b313664
Nikol <nikolhurd@gmail.com>
2024-07-14 19:29:49 +0200

    fixed adjusting grid

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const grid = document.querySelector("#grid");
+const btnSize = document.querySelector("#size");
 
-const createGrid = (number) => {
-  for (let i = 0; i < number; i++) {
+const createGrid = (size) => {
+  function intToPx(value) {
+    return value + "px";
+  }
+
+  const newSize = intToPx(400 / size);
+  for (let i = 0; i < size; i++) {
     const newRow = document.createElement("div");
     grid.appendChild(newRow);
+
+    newRow.style.width = newSize;
+    newRow.style.height = newSize;
+    newRow.style.border = "0.1px solid black";
+    newRow.style.backgroundColor = "lightgray";
     newRow.addEventListener("mouseover", () => {
       newRow.style.background = "blue";
     });
-    for (let j = 0; j < number - 1; j++) {
+    for (let j = 0; j < size; j++) {
       const newCol = document.createElement("div");
-      grid.appendChild(newCol);
+      newRow.appendChild(newCol);
+      newCol.style.width = newSize;
+      newCol.style.height = newSize;
+      newCol.style.border = "0.1px solid black";
+      newCol.style.backgroundColor = "lightgray";
       newCol.addEventListener("mouseover", () => {
         newCol.style.background = "blue";
       });
     }
   }
 };
-createGrid(16);
+createGrid(100);
+
+// btnSize.addEventListener("click", () => {
+//   createGrid(size);
+// });
+// let size = prompt("Enter number of square line (1 - 100)", 16);
